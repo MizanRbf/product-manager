3362e5febae826436aa7c186e313b5a896cdf39e
MizanRbf <mizanrbf@gmail.com>
2025-08-21 09:05:56 +0600

    banner section updated adding bg imgage and title,subtitle and two buttons

--- src/app/Components/Hero/Hero.jsx
+++ src/app/Components/Hero/Hero.jsx
@@ -2,9 +2,21 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="pt-[64px]">
-      <div className="max-w-[1500px] mx-auto px-4">
-        <h1>Hero</h1>
+    <div className="bg-[url('/assets/hero.jpg')] bg-cover bg-center bg-no-repeat py-40 flex items-center justify-center">
+      <div className="max-w-[1500px] mx-auto px-4 pt-[64px] text-center text-white space-y-4">
+        <h1 className="text-white">Manage Your Products Easily</h1>
+        <p className="bg-secondary rounded-full py-2">
+          A simple Next.js app with authentication and product management.
+        </p>
+        {/* Button */}
+        <div className="space-x-4">
+          <button className="px-6 py-1 rounded-sm bg-white text-primary font-bold text-lg cursor-pointer">
+            Get Started
+          </button>
+          <button className="px-6 py-1 rounded-sm bg-white text-primary font-bold text-lg cursor-pointer">
+            Explore Products
+          </button>
+        </div>
       </div>
     </div>
   );
